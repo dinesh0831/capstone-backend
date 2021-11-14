1cc1769bc28677748fa45c1b5aa3a4bfd2ec2a3b
dinesh0831 <81325951+dinesh0831@users.noreply.github.com>
2021-11-14 16:38:04 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -3,7 +3,7 @@ const mongoose=require("mongoose")
 exports.connect=async()=>{
     
     try{
-   await mongoose.connect("mongodb+srv://Dinesh:Dinesh31@cluster0.ed3jk.mongodb.net/capstone?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true})
+   await mongoose.connect(process.env.mongoose,{useNewUrlParser:true,useUnifiedTopology:true})
    
     console.log("success mongoose")
     }
